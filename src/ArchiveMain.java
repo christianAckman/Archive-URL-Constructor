469588147c9adad10a87824e2474786e96c6f178
Christian Ackman <ackmanChristian@gmail.com>
2017-01-23 10:37:35 -0500

    Update ArchiveMain.java

--- src/ArchiveMain.java
+++ src/ArchiveMain.java
@@ -6,13 +6,11 @@ public class ArchiveMain {
 		
 		Collect call = new Collect();
 		
-		String[] words = new String[6];
-		words[0] = "two";
-		words[1] = "pages";
-		words[2] = "one";
-		words[3] = "four";
-		words[4] = "three";
-		words[5] = "five";
+		String[] words = new String[4];
+		words[0] = "one";
+		words[1] = "nice";
+		words[2] = "long";
+		words[3] = "example";
 		
 		String[] URL = call.constructURL(words);	// URL needs: Page number
 		
